8925255b9f64000591056964cc000f5915af5b2d
max_ksiazka <maksymilian.ksiazka@gmail.com>
2026-05-16 20:06:44 +0200

    add: python mqtt subscriber

--- app/main.py
+++ app/main.py
@@ -1,7 +1,31 @@
+#pyright: basic
 from fastapi import FastAPI, status
 from .routers import iot_rest
+from contextlib import asynccontextmanager
+import aiomqtt
+import asyncio
 
-app = FastAPI(title="Iot test")
+async def mqtt_subscriber():
+    try:
+        async with aiomqtt.Client("mqtt-broker",1883) as client:
+            await client.subscribe("iot/esp32/tempsensors")
+            async for message in client.messages:
+                payload_str = message.payload.decode()
+                print(f"Received MQTT message: {payload_str}")
+                #db logic here
+    except Exception as e:
+        print(f"Error in MQTT subscriber: {e}")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up the application...")
+    mqtt_task = asyncio.create_task(mqtt_subscriber())
+    yield
+    mqtt_task.cancel()
+    print("Shutting down the application...")
+
+
+app = FastAPI(title="IoT-Backend",lifespan=lifespan)
 app.include_router(iot_rest.router,
                    prefix="/api/v1/iot"
                    )
