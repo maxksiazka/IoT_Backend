263d432dbfcce5491fea780992055bae4a94745a
max_ksiazka <maksymilian.ksiazka@gmail.com>
2026-05-26 15:45:34 +0200

    fix: wrong schema name

--- app/routers/iot_rest.py
+++ app/routers/iot_rest.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter,status,Request,HTTPException
 from app.db.database import write_sensor_data
-from app.schemas import SensorData
+from app.schemas.schemas import SensorReading
 router = APIRouter()
 
 @router.post("/data", status_code=status.HTTP_200_OK)
-async def data_post(req: Request, sensor_data: SensorData):
+async def data_post(req: Request, sensor_data: SensorReading):
     data_dict = sensor_data.model_dump()
     print(f"Received POST data: {data_dict}")
     req.app.state.latest_sensor_data[sensor_data.sensor_id] = data_dict
